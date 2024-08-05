0e880c660bc6bfdbe18a5d726e4173adc8799720
Douglas Chimento <dchimento@gmail.com>
2024-08-05 19:15:24 +0300

    pnpm update

--- apps/cctp/src/app/page.tsx
+++ apps/cctp/src/app/page.tsx
@@ -5,13 +5,24 @@ import {useNamespace} from "@/contexts/NamespaceContext";
 import Namespace from "@/components/Namespace";
 
 export default function Home() {
-  const {namespace} = useNamespace();
-  //{ namespace === undefined ? <Namespace/> : <Pair/> }
   return (
     <main>
-      <div>
-        <h1>whats up</h1>
-      </div>
+      <div className="item-center mx-auto flex max-w-4xl flex-col justify-center">
+        <h1>Transfer USDC across chains</h1>
+        <p className="mt-8 text-center text-xl">
+          Circle&apos;s Cross-Chain Transfer Protocol enables USDC to be sent
+          across blockchains without the need to be converted into a asset. All
+          transfers are permissionless and executed on-chain.
+        </p>
+        {/*}
+        <div className="m-24 flex flex-col">
+          <SendForm
+            handleNext={handleNext}
+            formInputs={formInputs}
+            handleUpdateForm={setFormInputs}
+          />
+          */}
+        </div>
     </main>
   )
 }
@@ -20,7 +31,8 @@ function OrgHome() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
